d3a34fda534b6bcff898f6639a9ab9cd990b71af
Amanda Rush <mandyrush85@gmail.com>
2019-11-25 15:01:33 -0700

    add functionality to add & remove ingredients & update price with build controls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,8 +12,14 @@ const controls = [
 
 const buildControls = (props) => (
     <div className={classes.BuildControls} >
+        <p><strong>Current Price: {props.price.toFixed(2)}</strong></p>
         {controls.map(ctrl => (
-            <BuildControl label={ctrl.label} key={ctrl.label} />
+            <BuildControl 
+                label={ctrl.label} 
+                key={ctrl.label} 
+                added={() => props.ingredientAdded(ctrl.type)}
+                removed={() => props.ingredientRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]} />
         ))}
     </div>
 );
